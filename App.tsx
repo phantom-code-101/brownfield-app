f6f90953d0d34e5e162e614c2a2178e768bec73a
Vincent Li <vincent101001@gmail.com>
2025-04-16 19:29:43 +0800

    register root compontent

--- App.tsx
+++ App.tsx
@@ -1,30 +1,4 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello from React Native!</Text>
-      <Text style={styles.subText}>This is a brownfield integration</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  text: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  subText: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+registerRootComponent('brownfield-app', () => App);
